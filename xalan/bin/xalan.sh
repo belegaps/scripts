002f5c3a00d22df3ad1b961b0d35add2e6c28e63
Per Salomonsen <pes@ccieurope.com>
2016-04-14 14:58:59 +0200

    Add serializer library to class path
    The Maven Xalan library doesn't include the serializer components.
    This has to be added specifically to the class path.

--- xalan/bin/xalan.sh
+++ xalan/bin/xalan.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-if [ ! -f ~/.m2/repository/xalan/xalan/2.7.2/xalan-2.7.2.jar ]; then
-	mvn dependency:get -Dartifact=xalan:xalan:2.7.2
+
+# Version of Xalan to use
+XALAN_VERSION=2.7.2
+
+# Required JAR files for executing processor
+XALAN_JAR=~/.m2/repository/xalan/xalan/$XALAN_VERSION/xalan-$XALAN_VERSION.jar
+SERIALIZER_JAR=~/.m2/repository/xalan/serializer/$XALAN_VERSION/serializer-$XALAN_VERSION.jar
+
+# Use Maven to download Xalan jar and it's dependency (serializer)
+if [ ! -f $XALAN_JAR -o ! -f $SERIALIZER_JAR ]; then
+	mvn dependency:get -Dartifact=xalan:xalan:$XALAN_VERSION
 fi
 
-java -cp ~/.m2/repository/xalan/xalan/2.7.2/xalan-2.7.2.jar org.apache.xalan.xslt.Process $*
+# Execute XML transformation processor
+java -cp $XALAN_JAR:$SERIALIZER_JAR org.apache.xalan.xslt.Process $*
